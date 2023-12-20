e27b773664946f1a217602f22414b46f2d751378
Veeravalli <avinash.veeravalli@dxc.com>
2023-12-20 23:05:29 +0800

    VM creation code added

--- main.tf
+++ main.tf
@@ -1,39 +1,76 @@
 terraform {
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
       version = "3.85.0"
     }
   }
-  backend "azurerm"{
-    resource_group_name = var.rg_name
-    storage_account_name = "tfstate193"
-    container_name = "tfstate"
-    key = "dev.tfstate"
-  }
+  # backend "azurerm"{
+
+  # }
 }
 
-
-
 provider "azurerm" {
-    features{}
+  features {}
 }
 
 resource "azurerm_resource_group" "rg" {
-    name = var.rg_name
-    location = var.rg_location
+  name     = var.rg_name
+  location = var.rg_location
 }
 
 resource "azurerm_virtual_network" "vnet" {
-    resource_group_name = resource.azurerm_resource_group.rg.name
-    name = var.vnet_name
-    location = resource.azurerm_resource_group.rg.location
-    address_space = [var.vnet_addr]
+  resource_group_name = resource.azurerm_resource_group.rg.name
+  name                = var.vnet_name
+  location            = resource.azurerm_resource_group.rg.location
+  address_space       = [var.vnet_addr]
 }
 
 resource "azurerm_subnet" "sn" {
-    name = var.sbnet_vm
-    resource_group_name = resource.azurerm_resource_group.rg.name
-    address_prefixes = [var.sbnet_vm_addr]
-    virtual_network_name = resource.azurerm_virtual_network.vnet.name
+  name                 = var.sbnet_vm
+  resource_group_name  = resource.azurerm_resource_group.rg.name
+  address_prefixes     = [var.sbnet_vm_addr]
+  virtual_network_name = resource.azurerm_virtual_network.vnet.name
+}
+
+resource "azurerm_network_interface" "nic" {
+  name                = "${var.vmname}-nic"
+  resource_group_name = resource.azurerm_resource_group.rg.name
+  location            = resource.azurerm_resource_group.rg.location
+
+  ip_configuration {
+    name                          = "${var.vmname}-internal"
+    private_ip_address_allocation = "Static"
+    subnet_id                     = azurerm_subnet.sn.id
+    public_ip_address_id          = resource.azurerm_public_ip.pip.id #We are mappin the public ip to the nic
+    }
+}
+
+resource "azurerm_public_ip" "pip" {
+  name                = "${var.vmname}-pip"
+  location            = resource.azurerm_resource_group.rg.location
+  resource_group_name = resource.azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+}
+
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                            = var.vmname
+  resource_group_name             = resource.azurerm_resource_group.rg.name
+  location                        = resource.azurerm_resource_group.rg.location
+  size                            = "Standard_F2"
+  admin_username                  = "loginadmin"
+  admin_password                  = "Password@123"
+  disable_password_authentication = false
+  network_interface_ids           = [azurerm_network_interface.nic.id]  # By specifying list [] we can add mulitiplpe ips
+   os_disk {
+    storage_account_type = "Standard_LRS"
+    caching              = "ReadOnly" # other possible values are "None", "ReadWrite"
+    disk_size_gb         = 20
+  }
+  source_image_reference {
+    offer     = "0001-com-ubuntu-server-jammy"
+    publisher = "Canonical"
+    sku       = "22_04-lts-gen2"
+    version   = "latest"
+  }
 }
--- variables.tf
+++ variables.tf
@@ -1,29 +1,34 @@
 variable "rg_name" {
-  type = string
+  type        = string
   description = "This variable is for resource group name"
 }
 
 variable "rg_location" {
-  type = string
+  type        = string
   description = "This variable is for resource group location"
 }
 
 variable "vnet_name" {
-    type = string
-    description = "This variable for virtual network name"
+  type        = string
+  description = "This variable for virtual network name"
 }
 
 variable "vnet_addr" {
-    type = string
-    description = "This variable is for virtual network name"
+  type        = string
+  description = "This variable is for virtual network name"
 }
 
 variable "sbnet_vm" {
-    type = string
-    description = "This variable is for VM subnet name"
+  type        = string
+  description = "This variable is for VM subnet name"
 }
 
 variable "sbnet_vm_addr" {
-    type = string
-    description = "This variable is for VM subnet address range"  
+  type        = string
+  description = "This variable is for VM subnet address range"
+}
+
+variable "vmname" {
+  type        = string
+  description = "This variable is for name of the VM"
 }
